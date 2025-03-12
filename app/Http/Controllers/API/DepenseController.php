3b481d8eccf2a3339da701c89bd52dc0d8e2960e
Hamza Saaf <hamzasaaf725@gmail.com>
2025-03-12 11:19:54 +0000

    creation du controller et Model pour Dépenses

---
+++ app/Http/Controllers/API/DepenseController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Depense;
+use Illuminate\Http\Request;
+
+class DepenseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Depense  $depense
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Depense $depense)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Depense  $depense
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Depense $depense)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Depense  $depense
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Depense $depense)
+    {
+        //
+    }
+}
